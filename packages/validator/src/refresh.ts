355e1779a658402e13ba6febd7930a065ce66f46
Edward Lancaster <edward.lancaster@siemens.com>
2023-07-18 09:53:22 +0200

    add eslint, use promises

--- packages/validator/src/refresh.ts
+++ packages/validator/src/refresh.ts
@@ -1,4 +1,17 @@
-// Thing Description Validator //
+/********************************************************************************
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
+ * Document License (2015-05-13) which is available at
+ * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
+ ********************************************************************************/
 
 // Imports //
 
@@ -6,14 +19,13 @@ import fs from 'fs';
 import Table from 'cli-table3'
 import { Command } from 'commander';
 import addFormats from 'ajv-formats';
-import Ajv from 'ajv';
-import tdSchema from './td-schema.json' assert { type: "json" };
-import tdSchemaFull from './td-schema-full.json' assert { type: "json" };
-import tmSchema from './tm-schema.json' assert { type: "json" };
+import Ajv, {JSONSchemaType} from 'ajv';
+import tdSchema from './td-schema.json';
+import tdSchemaFull from './td-schema-full.json';
+import tmSchema from './tm-schema.json';
 import jsonld, { JsonLdDocument } from "jsonld";
 import builder from "junit-report-builder";
 import ProgressBar from 'progress';
-import apply from 'ajv-formats-draft2019';
 
 // consts //
 
@@ -25,7 +37,7 @@ export interface Thing {
     fileName: string;
     fileTitle: string;
     fileString: string;
-    fileJSON: any;
+    json: any;
     isTM: boolean;
     valid: boolean;
     report: {
@@ -68,32 +80,34 @@ program.parse(process.argv);
 const options = program.opts();
 
 // Create array of files to validate //
-let paths = program.args;
-let files: string[] = [];
+const paths = program.args;
+const files: string[] = [];
 
+// Scan for subfolders and files //
 for (const path of paths) {
     scanFiles(path, files);
 }
 
 files.length === 1 ? console.log("Found 1 file to validate.") : console.log("Found " + files.length + " files to validate.");
 
+// Create progress bar //
 const bar = new ProgressBar('[:bar] :percent ', {
     total: files.length,
     width: 50
 });
 
 // Read files into Objects //
-let things: Thing[] = [];
-
+const things: Thing[] = [];
 for (const file of files) {
     things.push(createThingObject(file));
 }
 
+// Create Thing Object //
 export function createThingObject(file: string): Thing {
-    let thing: Thing = {
+    const thing: Thing = {
         fileName: file,
         fileString: fs.readFileSync(file, 'utf8'),
-        fileJSON: undefined,
+        json: undefined,
         isTM: false,
         valid: false,
         report: {
@@ -108,16 +122,31 @@ export function createThingObject(file: string): Thing {
     return thing;
 }
 
+const promises: Promise<boolean>[] = [];
+
+// Validate each Thing //
 for (const thing of things) {
-    thing.valid = await validateTD(thing);
-    bar.tick();
+    promises.push(validateTD(thing));
 }
 
-if (options.junit) {
+Promise.all(promises).then(() => {
+    console.log("Done!");
+    if (options.junit) {
+        createJunitReport(things);
+    }
+    printTable();
+    if (things.some(thing => !thing.valid)) {
+        process.exit(1);
+    }
+});
+
+
+// Generate JUnit report //
+function createJunitReport(things: Thing[]) {
     for (const thing of things) {
-        let suite = builder.testSuite().name(thing.fileTitle);
+        const suite = builder.testSuite().name(thing.fileTitle);
         for (const [key, value] of Object.entries(thing.report)) {
-            let testcase = suite.testCase()
+            const testcase = suite.testCase()
                 .name(key)
                 .time(value.time);
             switch (value.result) {
@@ -138,26 +167,28 @@ if (options.junit) {
     builder.writeTo('report.xml');
 }
 
+function printTable() {
+    // Table setup //
+    const table = new Table({
+        head: ['File', 'Type', 'JSON', 'Schema', 'Defaults', 'JSON-LD', 'Time'],
+        colWidths: [30, 6, 10, 10, 10, 10, 6, 5]
+    });
 
-const table = new Table({
-    head: ['File', 'Type', 'JSON', 'Schema', 'Defaults', 'JSON-LD', 'Time'],
-    colWidths: [30, 6, 10, 10, 10, 10, 6, 5]
-});
-
-things.forEach(thing => {
-    let type: string;
-    let valid: string;
-    if (thing.isTM) {
-        type = "TM";
-    } else {
-        type = "TD";
-    }
-    thing.valid ? valid = "✅" : valid = "❌";
-    table.push([thing.fileTitle, type, thing.report.json.result, thing.report.schema.result, thing.report.defaults.result, thing.report.jsonld.result, thing.time, valid]);
-});
-
-console.log(table.toString());
-
+    // Format and print output //
+    things.forEach(thing => {
+        let type: string;
+        let valid: string;
+        if (thing.isTM) {
+            type = "TM";
+        } else {
+            type = "TD";
+        }
+        thing.valid ? valid = "✅" : valid = "❌";
+        table.push([thing.fileTitle, type, thing.report.json.result, thing.report.schema.result, thing.report.defaults.result, thing.report.jsonld.result, thing.time, valid]);
+    });
+    console.log(table.toString());
+}
+// Scan for subfolders and files //
 function scanFiles(path: string, files: string[]) {
     if (!fs.existsSync(path)) {
         console.log("File or directory does not exist: " + path);
@@ -177,12 +208,14 @@ function scanFiles(path: string, files: string[]) {
     }
 }
 
+// Validate TD //
 export async function validateTD(thing: Thing): Promise<boolean> {
-    let time = Date.now();
+    const time = Date.now();
     // Parse JSON
     if (!parseJSON(thing)) {
         thing.time = Date.now() - time;
-        return false;
+        thing.valid = false;
+        Promise.resolve(false);
     }
 
     // Determine if TD or TM
@@ -191,26 +224,29 @@ export async function validateTD(thing: Thing): Promise<boolean> {
     // Validate against schema
     if (!validateSchema(thing)) {
         thing.time = Date.now() - time;
-        return false;
+        thing.valid = false;
+        Promise.resolve(false);
     }
 
     // Validate JSON-LD
     if (!options.offline) {
         if (!(await validateJsonLd(thing))) {
             thing.time = Date.now() - time;
-            return false;
+            thing.valid = false;
+            Promise.resolve(false);
         }
     }
     thing.time = Date.now() - time;
-
-
+    thing.valid = true;
+    bar.tick();
+    Promise.resolve(true);
     return true;
 }
 
 function parseJSON(thing: Thing): boolean {
-    let time = Date.now();
+    const time = Date.now();
     try {
-        thing.fileJSON = JSON.parse(thing.fileString);
+        thing.json = JSON.parse(thing.fileString);
         thing.report.json.result = Result.Valid;
         thing.report.json.time = Date.now() - time;
     } catch (e) {
@@ -224,7 +260,7 @@ function parseJSON(thing: Thing): boolean {
 }
 
 function checkThingType(thing: Thing) {
-    if (thing.fileJSON?.["@type"] === "tm:ThingModel") {
+    if (thing.json?.["@type"] === "tm:ThingModel") {
         thing.isTM = true;
     }
 }
@@ -233,13 +269,12 @@ function validateSchema(thing: Thing): boolean {
     // Create AJV instance
     let ajv = new Ajv({ strict: false });
     ajv = addFormats(ajv);
-    ajv = apply(ajv);
     thing.isTM ? ajv.addSchema(tmSchema, "tm") : ajv.addSchema(tdSchema, "td");
 
     // Validate against schema
-    let valid = ajv.validate(thing.isTM ? "tm" : "td", thing.fileJSON);
+    const valid = ajv.validate(thing.isTM ? "tm" : "td", thing.json);
     if (!valid) {
-        bar.interrupt(`Error validating JSON file in ${thing.fileTitle}: ` + ajv.errorsText());
+        bar.interrupt(`Error validating Schema in ${thing.fileTitle}: ` + ajv.errorsText());
         thing.report.schema.result = Result.Invalid;
         thing.report.schema.message = ajv.errorsText();
         return false;
@@ -247,7 +282,7 @@ function validateSchema(thing: Thing): boolean {
         thing.report.schema.result = Result.Valid;
         if (!thing.isTM) {
             ajv.addSchema(tdSchemaFull, "fulltd");
-            const fullValid = ajv.validate("fulltd", thing.fileJSON);
+            const fullValid = ajv.validate("fulltd", thing.json);
             if (fullValid) {
                 thing.report.defaults.result = Result.Valid;
             } else {
@@ -261,9 +296,9 @@ function validateSchema(thing: Thing): boolean {
 }
 
 async function validateJsonLd(thing: Thing): Promise<boolean> {
-    let time = Date.now();
+    const time = Date.now();
     try {
-        let jsonFile: JsonLdDocument = thing.fileJSON;
+        const jsonFile: JsonLdDocument = thing.json;
         await jsonld.toRDF(jsonFile, { format: "application/n-quads" });
         thing.report.jsonld.result = Result.Valid;
         thing.report.jsonld.time = Date.now() - time;
